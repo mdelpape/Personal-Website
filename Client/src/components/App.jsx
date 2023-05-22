ed6cfac628cc056d5ade5b39d85f22310c313a17
Michael Del Pape <m4delpape@gmail.com>
2023-05-21 18:12:17 -0700

    Rendered page

---
+++ Client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+export default function App() {
+  const [content, setContent] = useState()
+
+  return (
+    <div id="App">
+      <header>Michael Del Pape</header>
+
+    </div>
+  );
+}
