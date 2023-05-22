d7bd16ecc57a5155e202b6de98ceefc28138f80d
Michael Del Pape <m4delpape@gmail.com>
2023-05-21 23:25:48 -0700

    added git link

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -6,12 +6,18 @@ import Contact from './Contact.jsx';
 
 export default function App() {
   const home =
-    (<div id = 'home'>
-      <h1 id = 'homeName'>Michael Del Pape</h1>
-      <img id = 'moon' src = 'https://upload.wikimedia.org/wikipedia/commons/b/b8/Moon_rotating_full_160px.gif'/>
-    </div>
+    (
+    <div>
+      <div id = 'home'>
+        <h1 id = 'homeName'>Michael Del Pape</h1>
+        <img id = 'moon' src = 'https://upload.wikimedia.org/wikipedia/commons/b/b8/Moon_rotating_full_160px.gif'/>
+      </div>
+      <img id = 'spaceMan'src = 'https://i.gifer.com/X5NY.gif' width = '100px'/>
+      <img id = 'github' src = 'https://git-scm.com/images/logos/downloads/Git-Logo-White.png' width = '100px' onClick = {() => {
+        window.location.href = 'https://github.com/mdelpape'
+      }}/>
+    </div>)
 
-    )
 
   const [content, setContent] = useState(home);
 
@@ -27,7 +33,9 @@ export default function App() {
           <p id = 'navTitle' onClick = {() => {Contact(setContent)}}>Contact</p>
         </div>
       </div>
-      {content}
+      <div>
+        {content}
+      </div>
     </div>
   );
 }
